f6f79abff28ecdd4b23c86b75949ab013bff0032
ankush121998 <ankush121998@gmail.com>
2026-04-19 22:37:23 +0530

    added admin url, so that i can login as superuser admin

--- billing_platform/urls.py
+++ billing_platform/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("api/v1/", include("billing.interfaces.api.incoming.urls")),
 ]
